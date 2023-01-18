352415db22cc9471a2ade1d725a32783d525260b
Dev Luca <lucahesper@hotmail.com>
2023-01-18 13:37:57 -0300

    add server and interface directories

---
+++ app-web/src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-blue-300">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ server/prisma/migrations/20230117135033_create_habit_model/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
